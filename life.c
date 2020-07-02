162248e566904a09da3fcc261595366da2ff786e
Amircpu <email@amircpu.ir>
2020-07-03 03:13:25 +0430

    Complit Game life

--- life.c
+++ life.c
@@ -1,46 +1,75 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
+#include<unistd.h>
 
-void drown(void* world,int w,int h)
+void drow(void* wrd,int w,int h)
 {
-	char (*World)[w]=world;
+	//Temp World
+	char (*world)[w]=wrd;
 
-        for(int y=0; y<w ; y++)
+	//Clear Terminal
+	printf("\033[H");
+
+        for(int y=0; y<h ; y++)
         {
-                for(int x=0;x<h;x++)
+                for(int x=0;x<w;x++)
                 {
-                        printf("0");
+                        printf(world[y][x]? "*" : " ");
                 }
 		printf("\n");
         }
 }
 
-void enviroment(void *world,int w,int h)
+void evolution(void *wrd,int w,int h)
 {
-	char* new=world;
-	
-	for(int y=0; y<w ; y++)
+	char (*world)[w]=wrd;
+	char newWorld[h][w];
+
+	for(int y=0; y<h ; y++)
 	{
-		for(int x=0;x<h;x++)
+		for(int x=0;x<w;x++)
 		{
-			printf("0");
+			//state cell live count
+			int live = 0;
+
+			for(int yi=y-1; yi<y+1; yi++)
+				for(int xi=x-1; xi<x+1; xi++)
+					if(world[(yi+h)%h][(xi+w)%w])
+						live++;
+			if(world[y][x])
+				live--;
+
+			newWorld[y][x] = (live == 3 || (live==2 && world[y][x]));
 		}
-		printf("\n");
 	}
+
+	world = newWorld;
 }
 
 int main()
 {
 	//Initilze Value
-	int w = 30;//Length Width
-	int h = 30;//Length Hight
-	char world[w][h];//My World
-	
+	int w = 30;//Length culomns
+	int h = 30;//Length rows
+	char world[h][w];//My World
+
 	//Initilize new Matrix World
-	drown(world,w,h);
-	
-	//Check Life cell
-	enviroment(world,w,h);
+	for(int i=0; i<h; i++)
+		for(int j=0; j<w; j++)
+			world[i][j]=rand() < RAND_MAX / 10 ? 1 : 0 ;
+
+	for(;;)
+	{
+		//Show State matrix
+		drow(world,w,h);
+
+		//Check Life cell
+		evolution(world,w,h);
+
+		//sleep 1s
+		usleep(1000000);
+	}
 
 	return 0;
 }
